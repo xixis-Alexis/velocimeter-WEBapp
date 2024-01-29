1463c7d927ea1ccfe78fb1c518db88be31a8ff59
root <root@rp-f074a6.lan>
2024-01-29 10:03:35 +0000

    new file:   myFirstApp/Makefile 	new file:   myFirstApp/css/style.css 	new file:   myFirstApp/fpga.conf 	new file:   myFirstApp/fpga.sh 	new file:   myFirstApp/index.html 	new file:   myFirstApp/info/icon.png 	new file:   myFirstApp/info/info.json 	new file:   myFirstApp/js/app.js 	new file:   myFirstApp/js/jquery-2.1.3.min.js 	new file:   myFirstApp/js/pako.js 	new file:   myFirstApp/src/Makefile 	new file:   myFirstApp/src/main.cpp 	new file:   myFirstApp/src/main.h

---
+++ myFirstApp/js/app.js
@@ -0,0 +1,142 @@
+/*
+ * Red Pitaya Template Application
+ *
+ *
+ * (c) Red Pitaya  http://www.redpitaya.com
+ */
+
+
+(function(APP, $, undefined) {
+    
+    // App configuration
+    APP.config = {};
+    APP.config.app_id = 'myFirstApp';
+    APP.config.app_url = '/bazaar?start=' + APP.config.app_id + '?' + location.search.substr(1);
+    APP.config.socket_url = 'ws://' + window.location.hostname + ':9002';
+
+    // WebSocket
+    APP.ws = null;
+
+    APP.start_acq = false;
+    APP.SIGNAL = [];
+    APP.plot = "";
+
+    // Starts template application on server
+    APP.startApp = function() {
+
+        $.get(APP.config.app_url)
+            .done(function(dresult) {
+                if (dresult.status == 'OK') {
+                    APP.connectWebSocket();
+                } else if (dresult.status == 'ERROR') {
+                    console.log(dresult.reason ? dresult.reason : 'Could not start the application (ERR1)');
+                    APP.startApp();
+                } else {
+                    console.log('Could not start the application (ERR2)');
+                    APP.startApp();
+                }
+            })
+            .fail(function() {
+                console.log('Could not start the application (ERR3)');
+                APP.startApp();
+            });
+    };
+
+
+
+
+    APP.connectWebSocket = function() {
+
+        //Create WebSocket
+        if (window.WebSocket) {
+            APP.ws = new WebSocket(APP.config.socket_url);
+            APP.ws.binaryType = "arraybuffer";
+        } else if (window.MozWebSocket) {
+            APP.ws = new MozWebSocket(APP.config.socket_url);
+            APP.ws.binaryType = "arraybuffer";
+        } else {
+            console.log('Browser does not support WebSocket');
+        }
+
+
+        // Define WebSocket event listeners
+        if (APP.ws) {
+
+            APP.ws.onopen = function() {
+                $('#hello_message').text("Hello, Red Pitaya!");
+                console.log('Socket opened');               
+            };
+
+            APP.ws.onclose = function() {
+                console.log('Socket closed');
+            };
+
+            APP.ws.onerror = function(ev) {
+                $('#hello_message').text("Connection error");
+                console.log('Websocket error: ', ev);         
+            };
+
+            APP.ws.onmessage = function(ev) {
+                console.log('Message recieved');
+		try{
+			var data = new Uint8Array(ev.data);
+			var inflate = pako.inflate(data);
+			var text = String.fromCharCode.apply(null, new Uint8Array(inflate));
+			var receive = JSON.parse(text);
+			if(receive.signals){APP.SIGNAL.push(receive.signals)}
+			console.log(receive.signals)
+		}catch(e){
+			console.log(e)
+		}
+            };
+        }
+    };
+
+   APP.processSignals = function(new_signals){
+	var pointArr = [];
+	var voltage;
+	for (sig_name in new_signals){
+	
+	
+		if (new_signals[sig_name].size > 0){
+			voltage = new_signals[sig_name].value[0];
+			$('#AFF_int').text(parseFloat(voltage).toFixed(2));}
+			console.log(voltage);
+		}
+	}
+
+   APP.signalHandler = function(){
+	if (APP.SIGNAL.length > 0)
+	{
+		APP.processSignals(APP.SIGNAL[0]);
+	}
+	if(APP.SIGNAL.length > 2)
+		APP.SIGNAL.length = [];
+   }
+	setInterval(APP.signalHandler, 15);
+
+}(window.APP = window.APP || {}, jQuery));
+
+
+
+
+// Page onload event handler
+$(function() {
+    $('#start_acq').click(function(){
+        if(APP.start_acq == false){
+                APP.start_acq = true;
+                $('#AFF').text("TRUE");
+        }else{
+                APP.start_acq = false;
+                $('#AFF').text("FALSE");
+        }
+
+        var local = {};
+        local['START_ACQ'] = { value: APP.start_acq };
+        APP.ws.send(JSON.stringify({parameters: local}));
+   });
+
+    // Start application
+    APP.startApp();
+
+});
---
+++ myFirstApp/src/main.cpp
@@ -0,0 +1,102 @@
+#include <limits.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+
+#include "main.h"
+
+#include <DataManager.h>
+#include <CustomParameters.h>
+
+#define SIGNAL_SIZE_DEFAULT 5
+
+CBooleanParameter startAcq("START_ACQ", CBaseParameter::RW, false, 0);
+CFloatSignal signalTest("SIGNAL_TEST", SIGNAL_SIZE_DEFAULT, 0.0f);
+std::vector<float> g_data(SIGNAL_SIZE_DEFAULT);
+
+uint32_t pos;
+buffers_t* buf;
+
+const char *rp_app_desc(void)
+{
+    return (const char *)"Template application.\n";
+}
+
+
+int rp_app_init(void)
+{
+    fprintf(stderr, "Loading template application\n");
+    rp_Init();
+    //buf = rp_createBuffer(2, SIGNAL_SIZE_DEFAULT, false, false, true);
+    return 0;
+}
+
+
+int rp_app_exit(void)
+{
+    fprintf(stderr, "Unloading template application\n");
+    //rp_deleteBuffer(buf);
+    rp_Release();
+    return 0;
+}
+
+
+int rp_set_params(rp_app_params_t *p, int len)
+{
+    return 0;
+}
+
+
+int rp_get_params(rp_app_params_t **p)
+{
+    return 0;
+}
+
+
+int rp_get_signals(float ***s, int *sig_num, int *sig_len)
+{
+    return 0;
+}
+
+
+
+
+
+
+
+
+void UpdateSignals(void){
+	rp_AcqGetWritePointer(1, &pos);
+	//rp_AcqGetData(pos, buf);
+	//buff -> vector -> signal	
+	signalTest[0] = g_data[0];
+}
+
+
+void UpdateParams(void){
+   startAcq.Update();
+   if(startAcq.Value() == false)
+   {
+	rp_DpinSetState(RP_LED0, RP_LOW);
+	g_data[0] = 0.0f;
+	rp_AcqStop();
+   }
+   else
+   {
+	rp_DpinSetState(RP_LED0, RP_HIGH);
+	g_data[0] = 1.0f;
+   	rp_AcqStart();
+   }
+}
+
+
+void OnNewParams(void) {}
+
+
+void OnNewSignals(void){}
+
+
+void PostUpdateSignals(void){}
